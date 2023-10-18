a1339783991a1b9ee321c6b925efa609b157a5f9
Drishtant-Behera <108445843+Drishtant-Behera@users.noreply.github.com>
2023-10-17 23:45:03 -0400

    Added basic object following code.
    This code is not the real deal so I just substituted the real commands for print statements.

--- YOLO.v8_Model/test.py
+++ YOLO.v8_Model/test.py
@@ -1,4 +1,3 @@
-
 from ultralytics import YOLO
 import cv2
 from ultralytics.utils.plotting import Annotator 
@@ -9,6 +8,9 @@ cap.set(3, 640)
 cap.set(4, 480)
 
 while True:
+
+    x = False
+
     _, img = cap.read()
     
     results = model.predict(img)
@@ -26,10 +28,31 @@ while True:
             
             x1, y1, x2, y2 = box.xyxy[0]
 
-            print('Bounding box coordinates:', x1, y1, x2, y2)
-          
+            TrueX = (x2 + x1) / 2
+            DifX = x2 - x1
+
+    
+            if len(boxes) == 1:
+                x = True
+            
+            if x == True:
+                if DifX >= 200:
+                    print ("stop")
+
+                elif TrueX > 345:
+                    print ("turn right")
+                    
+                elif TrueX < 295:
+                    print("turn left")
+                    
+                elif 295 < TrueX < 345:
+                    print("move forward")
+
+    if x == False:
+        print("locate")
+
     img = annotator.result()
-    cv2.imshow('YOLO V8 Detection', img)     
+    cv2.imshow('YOLO V8 Detection', img)
     
     if cv2.waitKey(1) & 0xFF == ord(' '):
         break

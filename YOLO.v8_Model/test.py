1ab4d35e5b277a5c986e83aef3aa5ca7dd593d65
Drishtant-Behera <108445843+Drishtant-Behera@users.noreply.github.com>
2023-11-09 17:56:26 -0500

    removed unescesary import - ultralytics.utils.plotting import Annotator

--- YOLO.v8_Model/test.py
+++ YOLO.v8_Model/test.py
@@ -1,59 +1,69 @@
+#imports
 from ultralytics import YOLO
 import cv2
-from ultralytics.utils.plotting import Annotator 
 
-model = YOLO('best.pt')
+#this is my model but you can replace this model with any model and it should still work the same.
+model = YOLO('Object-Following-Car/best.pt')
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+# main loop
 while True:
-
     x = False
-
     _, img = cap.read()
-    
     results = model.predict(img)
 
     for r in results:
-        
-        annotator = Annotator(img)
-        
         boxes = r.boxes
         for box in boxes:
-            
             b = box.xyxy[0]
             c = box.cls
-            annotator.box_label(b, model.names[int(c)])
-            
-            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = map(int, b)  # Convert coordinates to integers
+
+            # Draw rectangle on the image
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
             TrueX = (x2 + x1) / 2
             DifX = x2 - x1
 
-    
             if len(boxes) == 1:
                 x = True
-            
-            if x == True:
+
+            #comands for car
+            if x:
                 if DifX >= 200:
-                    print ("stop")
+                    stop = True
+                    print("stop")
+                else:
+                    stop = False
 
-                elif TrueX > 345:
-                    print ("turn right")
-                    
-                elif TrueX < 295:
+                if TrueX > 345:
+                    print("turn right")
+                    right = True
+                else:
+                    right = False
+
+                if TrueX < 295:
                     print("turn left")
-                    
-                elif 295 < TrueX < 345:
+                    left = True
+                else:
+                    left = False
+
+                if 295 < TrueX < 345:
                     print("move forward")
+                    forward = True
+                else:
+                    forward = False
 
-    if x == False:
-        print("locate")
+        if not x:
+            print("locate")
+            locate = True
+        else:
+            locate = False
 
-    img = annotator.result()
     cv2.imshow('YOLO V8 Detection', img)
-    
+
     if cv2.waitKey(1) & 0xFF == ord(' '):
         break
 
